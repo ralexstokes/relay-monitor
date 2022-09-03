52db154bf91d2b759cb157a6c95c56abd0faddc4
Michael Neuder <mikeneuder@google.com>
2022-09-03 15:44:40 -0400

    don't export http.server field

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -16,26 +16,26 @@ type Config struct {
 type Server struct {
 	config *Config
 	logger *zap.Logger
-	Srv    *http.Server
+	srv    *http.Server
 }
 
 func New(config *Config, logger *zap.Logger) *Server {
 	return &Server{
 		config: config,
 		logger: logger,
-		Srv: &http.Server{
+		srv: &http.Server{
 			Addr: fmt.Sprintf("%s:%d", config.Host, config.Port),
 		},
 	}
 }
 
 func (s *Server) Run(mux *http.ServeMux) error {
-	s.Srv.Handler = mux
+	s.srv.Handler = mux
 	logger := s.logger.Sugar()
 	logger.Infof("API server listening on %s:%d", s.config.Host, s.config.Port)
-	return s.Srv.ListenAndServe()
+	return s.srv.ListenAndServe()
 }
 
 func (s *Server) Shutdown() error {
-	return s.Srv.Shutdown(context.Background())
+	return s.srv.Shutdown(context.Background())
 }
