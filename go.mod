9f03419ec0f8f284e27936d10902100f6574d732
Tomas Germanavicius <tomas@metrika.co>
2023-11-14 11:10:56 +0000

    Replace segmentio with librdkafka.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/cenkalti/backoff/v4 v4.2.1
+	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0
 	github.com/ethereum/go-ethereum v1.12.0
 	github.com/flashbots/go-boost-utils v1.6.0
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d
@@ -12,7 +13,6 @@ require (
 	github.com/protolambda/eth2api v0.0.0-20230316214135-5f8afbd6d05d
 	github.com/protolambda/zrnt v0.30.0
 	github.com/r3labs/sse/v2 v2.10.0
-	github.com/segmentio/kafka-go v0.4.43
 	go.uber.org/zap v1.24.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -52,7 +52,6 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
-	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/common v0.44.0 // indirect
@@ -69,11 +68,11 @@ require (
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- pkg/output/output.go
+++ pkg/output/output.go
@@ -4,22 +4,26 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"sync"
-	"time"
 
 	"github.com/ralexstokes/relay-monitor/pkg/config"
-	kafka "github.com/segmentio/kafka-go"
+	"go.uber.org/zap"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 type Output struct {
-	Path        string
-	f           *os.File
-	lock        sync.Mutex
-	kConf       *config.KafkaConfig
-	kafkaWriter *kafka.Writer
+	Path          string
+	f             *os.File
+	lock          sync.Mutex
+	kConf         *config.KafkaConfig
+	ctx           context.Context
+	producer *kafka.Producer
+	deliveryCh    chan kafka.Event
 }
 
-func NewFileOutput(filePath string, kafkaConfig *config.KafkaConfig) (*Output, error) {
+func NewFileOutput(ctx context.Context, filePath string, kafkaConfig *config.KafkaConfig) (*Output, error) {
 	// check and prepare file
 	err := CheckFile(filePath)
 	if err != nil {
@@ -39,14 +43,17 @@ func NewFileOutput(filePath string, kafkaConfig *config.KafkaConfig) (*Output, e
 
 	// check and prepare kafka producer
 	if kafkaConfig != nil {
-		output.kafkaWriter = &kafka.Writer{
-			Addr:         kafka.TCP(kafkaConfig.BootstrapServers...),
-			Topic:        kafkaConfig.Topic,
-			BatchBytes:   10 * 1024 * 1024, // 10MB
-			BatchSize:    20,
-			BatchTimeout: time.Second,
-			RequiredAcks: 1,
+		lp, err := kafka.NewProducer(&kafka.ConfigMap{
+			"bootstrap.servers":  strings.Join(kafkaConfig.BootstrapServers, ","),
+			"message.max.bytes":  10 * 1024 * 1024, // 10MB
+			"client.id":          "relay-monitor",
+			"enable.idempotence": true,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to initialize librdkafka producer: %w", err)
 		}
+		output.producer = lp
+		go output.consumeEvents()
 	}
 
 	return output, nil
@@ -59,16 +66,42 @@ func (o *Output) WriteEntry(entry []byte) error {
 	if err != nil {
 		return err
 	}
-	if o.kafkaWriter != nil {
-
-		ctx, cancel := context.WithTimeout(context.Background(), o.kConf.Timeout)
-		defer cancel()
-		err = o.kafkaWriter.WriteMessages(ctx, kafka.Message{Value: entry})
+	if o.producer != nil {
+		err := o.producer.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{
+				Topic:     &o.kConf.Topic,
+				Partition: kafka.PartitionAny,
+			},
+			Value: entry,
+		}, nil)
 		if err != nil {
 			return fmt.Errorf("could not write to kafka: %v", err)
 		}
 	}
 	return nil
+}
+
+func (o *Output) consumeEvents() {
+	for {
+		select {
+		case <-o.ctx.Done():
+			return
+		case e := <-o.producer.Events():
+			switch e := e.(type) {
+			case *kafka.Message:
+				if e.TopicPartition.Error != nil {
+					zap.S().Errorw("encountered unexpected error producing a message", zap.Error(e.TopicPartition.Error))
+				}
+			case kafka.Error:
+				if e.IsFatal() {
+					zap.S().Fatalw("kafka producer encountered fatal error", zap.Error(e))
+				} else {
+					zap.S().Errorw("kafka producer encountered an error", "error", e)
+				}
+			}
+		}
+
+	}
 }
 
 func (fo *Output) Close() error {
